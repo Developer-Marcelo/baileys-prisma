59afac3dfa9eaa1b35d9819d16e1c8ee36091efa
Marcelo Eduardo <desenvolvedoreasycode@gmail.com>
2025-12-22 17:55:14 -0300

    fix: relative paths in exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export { BaileysBeginner } from "@/libs/baileys-beginners";
+export { BaileysBeginner } from "./libs/baileys-beginners";
 export type {
   BrowserName,
   AdvancedWhatsappInterface,
   BasicWhatsappInterface,
   WhatsappInterface,
-} from "@/domain/whatsapp/whatsapp.interface";
+} from "./domain/whatsapp/whatsapp.interface";
 export type {
   BaileysEventHandler,
   MakeTransformedPrisma,
   MakeSerializedPrisma,
-} from "@/domain/whatsapp/whatsapp.types";
+} from "./domain/whatsapp/whatsapp.types";
